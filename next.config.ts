5f33086a7435d01ecd47ab89eb364012de9e2edf
XTIVI0658\mamde <mamde@xtivia.com>
2025-02-03 08:34:11 -0600

    added config options for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["images.ctfassets.net", "livedemo00.template-help.com"],
+    remotePatterns: [
+      {hostname:"livedemo00.template-help.com"},
+      {hostname:"images.ctfassets.net"}
+    ], 
+    // loader: "custom",
+  },
 };
 
 export default nextConfig;
