685f5820d6943f125ea12c77bb9a125e014e6dda
XTIVI0658\mamde <mamde@xtivia.com>
2025-02-04 08:09:10 -0600

    add sample project data for cards

---
+++ src/app/data.ts
@@ -0,0 +1,18 @@
+export const projects = [
+    {
+      description: "Create your own website to build a relationship with viewers and sell content.",
+      link: "https://i.vimeocdn.com/custom_asset/d3f1c74cd6c5cad8d89eebe38155c83d?q=80",
+      color: "#BBACAF"
+    },
+    {
+      description: "Create your own website to build a relationship with viewers and sell content.",
+      link: "https://i.vimeocdn.com/custom_asset/11e31eea5c20fe3dea4db10843435539?q=80",
+      color: "#666666"
+    },
+    {
+      description: "Create your own website to build a relationship with viewers and sell content.",
+      link: "https://i.vimeocdn.com/custom_asset/d0e8d77c10415983f00a8e8b8f0aea9f?q=80",
+      color: "#111134"
+    },
+    
+  ]
