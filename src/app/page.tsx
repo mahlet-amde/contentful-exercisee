6ced938a2b62cc9065011c848a1be760ba1f1017
XTIVI0658\mamde <mamde@xtivia.com>
2025-02-07 11:40:24 -0600

    fetch data from contentful and pass it to the components being rendered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,57 @@
-import React from "react";
-import Hero from "@/components/Hero";
+/* eslint-disable react/react-in-jsx-scope */
 import ImageOne from "@/../public/image_one.avif";
 import ImageTwo from "@/../public/image_two.avif";
 import ImageThree from "@/../public/image_three.avif";
 import { AppWindowIcon, ChartNoAxesCombinedIcon, HomeIcon } from "lucide-react";
-import Card from "@/components/Card";
+import { fetchEntries } from "@/lib/contentful";
 import LandingCard from "@/components/ui/LandingCard";
+import Hero from "@/components/Hero";
+import Card from "@/components/Card";
+import {
+  extractHeroFromCards,
+  extractLandingCards,
+} from "@/lib/utils/contentfulProcessor";
 
 export default async function Home() {
-  // const featureCard = await fetchEntries("featureCard");
-  // console.log("featureCard items: ", featureCard[0].fields);
+  const landingCards = await fetchEntries("landingCardList");
+  const featureCards = await fetchEntries("featureCardList");
 
-  // const icon = featureCard[0]?.fields?.icon?.fields?.file?.url;
-  // console.log("icon: ", icon);
+  const landingHero = extractHeroFromCards(landingCards[0]);
+  const featureHero = extractHeroFromCards(featureCards[0]);
+  const landingCard = landingCards[0];
 
-  // const description = featureCard[0]?.fields?.description?.content[0]?.content[0]?.value;
-  // console.log("Description ", description);
-
-  const featureHero = 
-    {
-      eyebrow:"Turn viewers into paying subscribers",
-      tite: "Video monetization platform",
-    }
-  
-
-  const landingHero = 
-    {
-      eyebrow:"Use Cases",
-      tite: "How customers use Vimeo"
-    }
-  
+  const cardData = extractLandingCards(landingCard);
 
   const cards = [
     {
       icon: HomeIcon,
-      description: "Create your own website to build a relationship with viewers and sell content.",
+      description:
+        "Create your own website to build a relationship with viewers and sell content.",
       image: ImageOne,
     },
     {
       icon: AppWindowIcon,
-      description: "Sell on-demand content globally and accept payment in over 100 currencies.",
+      description:
+        "Sell on-demand content globally and accept payment in over 100 currencies.",
       image: ImageTwo,
     },
     {
       icon: ChartNoAxesCombinedIcon,
-      description: "Offer incentives like free trials, exclusive content, and promotions to grow your following.",
+      description:
+        "Offer incentives like free trials, exclusive content, and promotions to grow your following.",
       image: ImageThree,
     },
   ];
 
   return (
     <main className="relative">
-      <Hero eyebrow={featureHero.eyebrow} title={featureHero.tite} />
+      <Hero eyebrow={featureHero.eyebrow} title={featureHero.title} />
+
       {cards.map((card, i) => (
         <Card key={`p_${i}`} i={i} {...card} />
       ))}
-      <Hero eyebrow={landingHero.eyebrow} title={landingHero.tite}/>
-      <LandingCard  />
+      <Hero eyebrow={landingHero.eyebrow} title={landingHero.title} />
+      <LandingCard cards={cardData} />
     </main>
   );
 }
