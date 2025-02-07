7a49dcf28f42c7556498e15c20eef83907d00626
XTIVI0658\mamde <mamde@xtivia.com>
2025-02-07 11:39:41 -0600

    code clean up and use eslint to fix build error

--- src/components/ui/LandingCard.tsx
+++ src/components/ui/LandingCard.tsx
@@ -1,36 +1,28 @@
+/* eslint-disable react/react-in-jsx-scope */
 "use client";
-
 
 import { useState } from "react";
 import Image from "next/image";
-import { VideoModal } from "../video-modal";
 
-const LandingCard = ({cards}: any) => {
+
+const LandingCard = ({ cards }: any) => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  const [modalOpen, setModalOpen] = useState<number | null>(null)
-  const openVideoModal = (index: number) => {
-    setModalOpen(index)
-  }
-  const closeVideoModal = () => {
-    setModalOpen(null)
-  }
- 
 
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="flex flex-wrap sm:flex-nowrap sm:justify-between gap-4 max-w-7xl mx-auto ">
-      {cards.map((card, index) => (
+      {cards.map((card: any, index: number) => (
         <div
           key={index}
           className={`block transition-all duration-300 ease-in-out 
         ${
-            hoveredIndex === index
+          hoveredIndex === index
             ? "sm:w-[calc(50%-0.5rem)]"
             : "sm:w-[calc(25%-0.75rem)]"
         }
         w-full mb-4 sm:mb-0 cursor-youtube-play`}
           onMouseEnter={() => setHoveredIndex(index)}
           onMouseLeave={() => setHoveredIndex(null)}
-            onClick={() => openVideoModal(index)}
         >
           <div
             className={`relative bg-[#141a20] rounded-3xl  flex flex-col-reverse shadow-2xl shadow-black `}
@@ -45,7 +37,7 @@ const LandingCard = ({cards}: any) => {
             </div>
             <div className="relative w-full h-96 ">
               <Image
-                src={card.imageUrl}
+                src={"https:" + card.url}
                 alt={card.title}
                 fill
                 style={{ objectFit: "cover" }}
@@ -55,7 +47,7 @@ const LandingCard = ({cards}: any) => {
           </div>
         </div>
       ))}
-    
+ 
     </div>
   );
 };
