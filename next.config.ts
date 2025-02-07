e43597780eb4c5a1b3611a4762287718be77d7e0
XTIVI0658\mamde <mamde@xtivia.com>
2025-02-07 11:38:25 -0600

    add eslint package and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
   images: {
     domains: ["images.ctfassets.net", "livedemo00.template-help.com"],
     remotePatterns: [
       {hostname:"livedemo00.template-help.com"},
-      {hostname:"images.ctfassets.net"},
+      {
+        hostname:"images.ctfassets.net"},
       {hostname:"i.vimeocdn.com"},
       {hostname:"assets.ctfassets.net"}
-      
       
     ], 
     // loader: "custom",
